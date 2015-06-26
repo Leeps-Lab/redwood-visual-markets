cc78f961fcd1338d40fe93defed3ad6f35a69160
echockel <echockel@ucsc.edu>
2015-06-25 19:17:14 -0700

    Create wait.js

---
+++ wait.js
@@ -0,0 +1,5 @@
+Redwood.controller("SubjectCtrl", ["$rootScope", "$scope", "RedwoodSubject", function($rootScope, $scope, rs) {
+  rs.on_load(function() { //called once the page has loaded for a new sub period
+    rs.next_period();
+	});
+}]);
